9817c6f1110aaeb2d2b8f7151d5a625b20a5bf4f
Stevekama <stevekamahertz@gmail.com>
2022-03-07 04:17:27 +0300

    Splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:endesha_groceries/helpers/iconhelper.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,29 +13,87 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Endesha Groceries',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
+        primaryColor: const Color(0xFF5B16D0),
       ),
-      home: const SplashScreen(),
+      home: SplashScreen(
+        duration: 3,
+        goToPage: const WelcomePage(),
+      ),
+    );
+  }
+}
+
+class IconFont extends StatelessWidget {
+  Color color;
+  double size;
+  String iconName;
+
+  IconFont({
+    Key? key,
+    required this.color,
+    required this.size,
+    required this.iconName,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      this.iconName,
+      style: TextStyle(
+        color: this.color,
+        fontSize: this.size,
+        fontFamily: 'orilla',
+      ),
     );
   }
 }
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+  int duration = 0;
+  Widget goToPage;
+
+  SplashScreen({
+    Key? key,
+    required this.duration,
+    required this.goToPage,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: this.duration), () {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => this.goToPage),
+        (route) => false,
+      );
+    });
+
+    return Scaffold(
+      body: Container(
+        color: const Color(0xFF5B16D0),
+        alignment: Alignment.center,
+        child: IconFont(
+          color: Colors.white,
+          size: 100.0,
+          iconName: IconFontHelper.LOGO,
+        ),
+      ),
+    );
+  }
+}
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: const Color(0xFF5B16D0),
-        child: const Center(
-          child: Icon(
-            Icons.favorite,
-            color: Colors.white,
-            size: 100,
-          ),
-        ),
+        alignment: Alignment.center,
+        child: const Text("Hello There?"),
       ),
     );
   }
